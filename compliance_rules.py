6eb8226cabf282d829e65cf8e7ea324adac49d58
TravisMa07 <travis07ma@gmail.com>
2025-08-11 21:00:53 -0400

    compliance_rules complete and compliance_report JSON output

--- compliance_rules.py
+++ compliance_rules.py
@@ -21,7 +21,7 @@ def check_storage_account_compliance(storage_account):
     }
 
 # compliance checks for virtual machines
-def check_virtual_machine_compliance(vm, nsg):
+def check_virtual_machine_compliance(vm, nsgs):
     tags = vm.get("tags", {})
     public_ips = len(vm.get("public_ips", [])) > 0
 
@@ -56,18 +56,79 @@ def check_virtual_machine_compliance(vm, nsg):
         
     }
 
-# LATER IMPLEMENTATION: compliance checks for Network Security Groups
-# LATER IMPLEMENTATION: evaluate compliance for all resources
-# LATER IMPLEMENTATION: Main function
-'''
+# compliance checks for network security groups
+# checks for overly permissive inbound rules on critical ports (22, 3389, 80, 443) which include:
+# - source_address_prefix is "*"
+# - access is "Allow"
+# - direction is "Inbound"
+# returns count of permissive rules and details of each rule
 def check_network_security_group_compliance(nsg):
+    
+    permissive_rules = []
+    check_ports = ["22", "3389", "80", "443"]
 
+    for rule in nsg.get("inbound_rules", []):
+        if (rule.get("source_address_prefix") == "*" 
+            and rule.get("access") == "Allow" 
+            and rule.get("direction") == "Inbound" 
+            and rule.get("destination_port_range") in check_ports):
+
+            # append relevant details of rules that been found to be overly permissive
+            # append dictionary with key pair values for name, source_address, and destination_port to permissive_rules list
+            permissive_rules.append({"name": rule.get("name"), 
+                                    "source_address": rule.get("source_address_prefix"), 
+                                    "destination_port": rule.get("destination_port_range")})
+            
+    return {
+        "permissive_rules_count": len(permissive_rules),
+        "permissive rules details": permissive_rules
+    }
+    
+
+
+# Evaluate compliance for all resources
+# take dictionary of Azure resources (from JSON file) and evaluate compliance for each resource type
+# returns name and compliance status for each resource type
+# iterate through all resources type and check compliance using the defined functions above
 def evaluate_compliance(resources):
+    compliance_report = { "storage_accounts": [], "virtual_machines": [], "network_security_groups": []}
+
+    # filter each resource type using list comprehensions
+    # the input 'resources' is a flat list of dictionaries, where each dictionary represents a resource and includes a 'type' key
+    # Since the data is a mixed flat list (not grouped/nested by resource type), we cannot access resources by type directly.
+    # so directly accessing keys like 'resource.get("type")' won't work on a list, therefore we filter the list to separate resources by their 'type'
+    storage_accounts = [r for r in resources if r.get("type") == "Storage Account"]
+    virtual_machines = [r for r in resources if r.get("type") == "Virtual Machine"]
+    network_security_groups = [r for r in resources if r.get("type") == "Network Security Group"]
+    
+
+    for storage_account in storage_accounts:
+        compliance_report["storage_accounts"].append({
+            "name": storage_account.get("name"),
+            "compliance": check_storage_account_compliance(storage_account)
+        })
+    for virtual_machine in virtual_machines:
+        nsgs = network_security_groups
+        compliance_report["virtual_machines"].append({
+            "name": virtual_machine.get("name"),
+            "compliance": check_virtual_machine_compliance(virtual_machine, nsgs)
+        })
+    for nsg in network_security_groups:
+        compliance_report["network_security_groups"].append({
+            "name": nsg.get("name"),
+            "compliance": check_network_security_group_compliance(nsg)
+        })
+
+    return compliance_report
+
+
 
 
 if __name__ == "__main__":
     resources = open_json_file('azure_resources.json')
     compliance_report = evaluate_compliance(resources)
-    print(json.dumps(compliance_report, indent=4))
-        
-'''
+    with open('compliance_report.json', 'w') as report_file:
+        json.dump(compliance_report, report_file, indent=4)
+ 
+
+
