9fe49b5bf64aaa3f7cfa685d3011b17cc94bfc76
TravisMa07 <travis07ma@gmail.com>
2025-08-13 21:02:44 -0400

    WIP terraform_remediation

--- terraform_remediation.py
+++ terraform_remediation.py
@@ -1,12 +1,13 @@
-import os
 import subprocess
 
+# This script is used to run Terraform commands from Python and pass variables to the configuration.
+# It initializes the Terraform directory, plans the infrastructure changes, and applies them through the script instead of manually running Terraform commands.
+
+# Path to the Terraform directory where commands will be executed
 terraform_directory = "../terraform/root"
-log_directory = "../terraform/runs"
-os.makedirs(log_directory, exist_ok=True)
-log_file_path = os.path.join(log_directory, "terraform_remediation.log")
 
-
+# Terraform variables to be passed to the configuration
+# tfvars dictionary contains the variables and their values which prevent manual input during Terraform execution
 tfvars = {
     "resource_group_name": "CSPA-Project-RG",
     "vm_name": "CSPA-VM1",
@@ -16,13 +17,21 @@ tfvars = {
     "owner_tag": "Travis Ma"
 }
 
-def terraform_command(command, cwd=terraform_directory):
-    pass
+# Convert a tfvars dictionary of Terraform variables and value into a list of CLI arguments
+#           Terraform CLI expects variable in the format: -var="key=value"
+# The function takes a dictionary and returns a list of string in where each string is a CLI argument (-var="key=value") for Terraform commands
+def tfvars_to_vars(tfvars):
+    cli_args = []
+    for key, value in tfvars.items():\
+    #   value
+        pass
 
 
-def main():
-    pass
-    terraform_command("terraform", "init")
-    terraform_command("terraform", "plan")
-    terraform_command("terraform", "apply")
+tfvars_terraform_cli_args = tfvars_to_vars(tfvars)
 
+
+subprocess.run(["terraform", "init"], cwd=terraform_directory)
+subprocess.run(["terraform", "plan"] #value)
+subprocess.run(["terraform", "apply"] #value)
+               
+print("Terraform commands executed successfully.")
